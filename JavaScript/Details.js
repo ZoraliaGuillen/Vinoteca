caada2b1f0983599469894977630311593157df2
ZoraliaGuillen <zguillena@gmail.com>
2023-05-16 23:33:14 +0200

    Actualización

---
+++ JavaScript/Details.js
@@ -0,0 +1,89 @@
+let vinos;
+
+async function getData() {
+    let datosApi
+    await fetch("/recursos/wines.JSON")
+        .then(response => response.json())
+        .then(json => datosApi = json)
+
+        vinos = datosApi.tipesOfWines;
+
+        imprimir()
+}
+
+getData()
+
+function imprimir() {
+
+    let id = location.search.split("?id=").filter(Number)
+    let selectId = id[0]
+    let Detalles = []
+
+    for (var i = 0; i < vinos.length; i++) {
+
+        if (vinos[i].id==selectId) {
+            Detalles.push(vinos[i])
+        }
+    }
+
+    var tarjetasVinosDetalles = document.getElementById("allWinesDetails")
+    tarjetasVinosDetalles.innerHTML = `<div class="cardContainerDetails">
+    <div class="imageCardDetails">
+        <i class="fa-solid fa-heart"></i>
+        <img src="../recursos/Images/${Detalles[0].Image}.png" alt="" class="wine">
+        <img src="../recursos/Images/${Detalles[0].Bandera}.png" alt="" class="flag">
+    </div>
+    <div class="cardDetails">
+        <h3 class="name">${Detalles[0].Nombre}</h3>
+        <p class="winery">${Detalles[0].Bodega}</p>
+        <strong>
+            <p class="price">${Detalles[0].Precio}0$</p>
+        </strong>
+        <div class="buttonsDetail">
+            <div class="quantity">
+                <button>-</button>
+                <div>1</div>
+                <button>+</button>
+            </div>
+            <button class="shoppingCart">AGREGAR A CARRITO <i class="fa-solid fa-cart-shopping color"></i></button>
+        </div>
+        <div class="winesDetails">
+            <h3>Detalles</h3>
+            <div>
+                <h4>Nombre :</h4>
+                <p>${Detalles[0].Nombre}</p>
+            </div>
+            <div>
+                <h4>Bodega :</h4>
+                <p>${Detalles[0].Bodega}</p>
+            </div>
+            <div>
+                <h4>País :</h4>
+                <p>${Detalles[0].Pais}</p>
+            </div>
+            <div>
+                <h4>Región :</h4>
+                <p>${Detalles[0].Region}</p>
+            </div>
+            <div>
+                <h4>Tipo de Vino :</h4>
+                <p>${Detalles[0].TipoDeVino}</p>
+            </div>
+            <div>
+                <h4>Uva :</h4>
+                <p>${Detalles[0].Uva}</p>
+            </div>
+            <div>
+                <h4>Cosecha :</h4>
+                <p>${Detalles[0].Cosecha}</p>
+            </div>
+            <div>
+                <h4>Puntuación :</h4>
+                <p>${Detalles[0].Puntuacion}</p>
+            </div>
+        </div>
+    </div>
+</div>
+    `
+}
+
---
+++ JavaScript/index.js
@@ -0,0 +1,139 @@
+let vinos;
+let tarjetasVinos = document.getElementById("allWines");
+let navVinos = document.getElementsByClassName("navLink");
+
+async function getData() {
+    let data;
+    await fetch("/recursos/wines.JSON")
+        .then(response => response.json())
+        .then(json => data = json)
+
+    vinos = data.tipesOfWines;
+
+    imprimir()
+
+}
+
+getData();
+
+for (let i = 0; i < navVinos.length; i++) {
+    let element = navVinos[i];
+    element.addEventListener("click", function (e) {
+        imprimir(e.target.id)
+    })
+}
+
+async function imprimir(id) {
+
+    switch (id) {
+
+        case "vinos":
+            home.style.display = "none"
+            wines.style.display = "none"
+            tipesOfWines.style.display = "none"
+            countries.style.display = "none"
+            recommendedWines.style.display = "none"
+            newslatter.style.display = "none"
+            allWines.style.display = "flex"
+            pintarHTML(vinos)
+            break;
+
+        case "vinosTintos":
+            let vinosTintos = vinos.filter(vino => vino.TipoDeVino == "Tinto");
+            home.style.display = "none"
+            wines.style.display = "none"
+            tipesOfWines.style.display = "none"
+            countries.style.display = "none"
+            recommendedWines.style.display = "none"
+            newslatter.style.display = "none"
+            allWines.style.display = "flex"
+            pintarHTML(vinosTintos)
+            break;
+
+        case "vinosBlancos":
+            let vinosBlancos = vinos.filter(vino => vino.TipoDeVino == "Blanco");
+            home.style.display = "none"
+            wines.style.display = "none"
+            tipesOfWines.style.display = "none"
+            countries.style.display = "none"
+            recommendedWines.style.display = "none"
+            newslatter.style.display = "none"
+            allWines.style.display = "flex"
+            pintarHTML(vinosBlancos)
+            break;
+
+        case "vinosRosados":
+            let vinosRosados = vinos.filter(vino => vino.TipoDeVino == "Rosado");
+            home.style.display = "none"
+            wines.style.display = "none"
+            tipesOfWines.style.display = "none"
+            countries.style.display = "none"
+            recommendedWines.style.display = "none"
+            newslatter.style.display = "none"
+            allWines.style.display = "flex"
+            pintarHTML(vinosRosados)
+            break;
+
+        case "vinosEspumosos":
+            let vinosEspumosos = vinos.filter(vino => vino.TipoDeVino == "Espumoso");
+            home.style.display = "none"
+            wines.style.display = "none"
+            tipesOfWines.style.display = "none"
+            countries.style.display = "none"
+            recommendedWines.style.display = "none"
+            newslatter.style.display = "none"
+            allWines.style.display = "flex"
+            pintarHTML(vinosEspumosos)
+            break;
+
+        case "contacto":
+            home.style.display = "none"
+            wines.style.display = "none"
+            tipesOfWines.style.display = "none"
+            countries.style.display = "none"
+            recommendedWines.style.display = "none"
+            newslatter.style.display = "none"
+            allWines.style.display = "flex"
+            pintarHTML(contacto);
+            break;
+
+        default:
+            allWines.style.display = "none"
+
+    }
+}
+
+function pintarHTML(array) {
+
+    let html = "";
+    for (var i = 0; i < array.length; i++) {
+        html +=
+            `
+        <div class="cardContainerWiness">
+            <div class="cardWines">
+                <i class="fa-solid fa-heart"></i>
+                <img src="./recursos/Images/${array[i].Image}.png" alt="${array[i].Nombre}">
+                <img src="./recursos/Images/${array[i].Bandera}.png" alt="" class="flag">
+                <p class="score">${array[i].Puntuacion}</p>
+                <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
+                class="fa-solid fa-star"></i><i class="fa-solid fa-star-half-stroke"></i>
+                <h3 class="name">${array[i].Nombre}</h3>
+                <p class="winerys">${array[i].Bodega}</p>
+                <strong>
+                    <p class="price">${array[i].Precio}</p>
+                </strong>
+                <div class="buttons">
+                    <button><a href="./HTML/Details.html?=id=${array[i].Id}.html">VER DETALLE</a></button>
+                    <button><a href="">AGREGAR</a></button>
+                </div>
+            </div>
+        <div>
+        `
+    }
+
+    tarjetasVinos.innerHTML = html;
+
+}
+
+
+
